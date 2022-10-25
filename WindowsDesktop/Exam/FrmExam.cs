96aa8710d6067352b0f1ba28cc9382d6ea819429
Shakikul Islam <shakikulislam625010@gmail.com>
2022-10-25 16:08:28 +0600

    Add SeatPlan

--- WindowsDesktop/Exam/FrmExam.cs
+++ WindowsDesktop/Exam/FrmExam.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using WindowsDesktop.Common;
+using WindowsDesktop.Theme;
 
 namespace WindowsDesktop.Exam
 {
     public partial class FrmExam : Form
     {
+        private Form _currentChildForm;
+
         public FrmExam()
         {
             InitializeComponent();
+
+            ThemeTemplate.LoadTheme(panelTopMenu);
+        }
+        
+        private void buttonExamSchedule_Click(object sender, System.EventArgs e)
+        {
+            GlobalSettings.OpenChildForm(new FrmSeatPlan(), _currentChildForm, panelBody);
         }
     }
 }
---
+++ WindowsDesktop/Exam/FrmSeatPlan.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using WindowsDesktop.Theme;
+
+namespace WindowsDesktop.Exam
+{
+    public partial class FrmSeatPlan : Form
+    {
+        public FrmSeatPlan()
+        {
+            InitializeComponent();
+            ThemeTemplate.LoadTheme(this);
+        }
+    }
+}
